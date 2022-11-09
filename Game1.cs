951cd8ba05955313ebd7b2e4209a7e63a18dd12b
Wilgot <wilgotwardell@hotmail.com>
2022-11-09 22:07:33 +0100

    -

--- Game1.cs
+++ Game1.cs
@@ -13,9 +13,12 @@ namespace TestGame
     {
         Texture2D ballTexture;
         Vector2 ballPosition;
+        public static Vector2 dir;
         float ballSpeed;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Instructor instruction1;
 
         public Game1()
         {
@@ -26,14 +29,15 @@ namespace TestGame
 
         protected override void Initialize()
         {
+            dir = new Vector2(0, 0);
             // TODO: Add your initialization logic here
             List<String> inst = new List<string>() { "l1", "w5", "u1", "d10", "u4" };
 
-            Instructor instruction1 = new Instructor(inst);
+            instruction1 = new Instructor(inst);
             instruction1.createInstructions();
 
             ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
-            ballSpeed = 100f;
+            ballSpeed = 40f;
 
             base.Initialize();
         }
@@ -42,6 +46,26 @@ namespace TestGame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             ballTexture = Content.Load<Texture2D>("ball");
+        }
+
+        public static void changeDir (char direction){
+           
+            switch (direction)
+            {
+                case 'r':
+                    dir = new Vector2(1, 0);
+                    break;
+                case 'l':
+                    dir = new Vector2(-1, 0);  
+                    break;
+                case 'u':
+                    dir = new Vector2(0, -1);
+                    break;
+                case 'd':
+                    dir = new Vector2(0, 1); 
+                    break;
+            }
+            
         }
 
         protected override void Update(GameTime gameTime)
@@ -53,31 +77,22 @@ namespace TestGame
             //
 
 
+            // ballPosition = instruction1.ob;
 
-            ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
+            ballPosition.Y += ballSpeed * dir.Y * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            ballPosition.X += ballSpeed * dir.X * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             // TODO: Add your update logic here
             // var kstate = Keyboard.GetState();
 
             // if (kstate.IsKeyDown(Keys.Up))
             // {
-            //     ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            // }
-
-            // if (kstate.IsKeyDown(Keys.Down))
-            // {
-            //     ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            // }
-
-            // if (kstate.IsKeyDown(Keys.Left))
-            // {
-            //     ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //     changeDir(true);
             // }
 
             // if (kstate.IsKeyDown(Keys.Right))
             // {
-            //     ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //     changeDir(false);
             // }
 
             base.Update(gameTime);
--- Instructor.cs
+++ Instructor.cs
@@ -6,27 +6,36 @@ using Microsoft.Xna.Framework.Input;
 using static System.Net.Mime.MediaTypeNames;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Timers;
+
 
 namespace TestGame
 {
-    internal class Instructor
+    public class Instructor
     {
-
-
+        
+        
         public List<String> instructions = new List<string>();
+        // public Vector2 ob {get; set;}
+        // public int obSpeed {get; set;}
+        
         // public Vector2 ballPos { get; set; }
         public Instructor(List<String> instructions)
         {
             this.instructions = instructions;
-
+            // this.ob = ob;
+            // this.obSpeed = obSpeed;
         }
 
         public void createInstructions()
         {
             for (int i = 0; i < instructions.Count; i++)
-            {
-                createSection(instructions[i]);
+            {    
+                createSection(instructions[i]);  
+                createInstructions(); 
             }
+            
         }
 
         public void createSection(string instruction)
@@ -39,22 +48,27 @@ namespace TestGame
                 amount = int.Parse(instruction.Remove(0, 1));
                 amount++;
             }
+            
             switch (firstLetter)
             {
                 case 'r':
                     printInstruction("Moving Right", amount);
+                    Game1.changeDir('r');
                     break;
                 case 'l':
                     printInstruction("Moving Left", amount);
+                    Game1.changeDir('l');
                     break;
                 case 'u':
                     printInstruction("Moving Up", amount);
+                    Game1.changeDir('u');
                     break;
                 case 'd':
                     printInstruction("Moving Down", amount);
+                    Game1.changeDir('d');
                     break;
             }
-
+            
         }
 
         public void printInstruction(string text, int amount)
